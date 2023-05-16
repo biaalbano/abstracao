9603e352731bcc18b44e06617432abbbe129942b
biaalbano <biaahalbano78@gmail.com>
2023-05-16 15:14:14 +0000

    gravar

---
+++ Quadrado.java
@@ -0,0 +1,15 @@
+public class Quadrado {
+    double calcularArea (){
+        
+        area = lado*lado;
+        
+        return area;
+    }
+     double calcularPerimetro (){
+        
+        perimetro = 4*lado;
+        
+        return perimetro;
+    }
+     
+}
